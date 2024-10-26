f238d33766aeba0ff7bd79acca8eec9de1ef0d34
Abdullah1Alnami <bdallhsydalnmy@gmail.com>
2024-10-26 22:17:58 +0300

    final updates

--- searching_methods.cpp
+++ searching_methods.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
-#include<String>
+#include <string>
 
 using namespace std;
+
 int Sequential_Search(int key, int arr[], int size) {
     for (int i = 0; i < size; i++) {
         if (key == arr[i]) return i;
@@ -10,11 +11,11 @@ int Sequential_Search(int key, int arr[], int size) {
 }
 
 int Recursive_Sequential_Search(int key, int arr[], int index, int size) {
-    
-    if (index >= size) return -1; 
-    if (arr[index] == key) return index; 
 
-    return Recursive_Sequential_Search(key, arr, index + 1, size);  
+    if (index >= size) return -1;
+    if (arr[index] == key) return index;
+
+    return Recursive_Sequential_Search(key, arr, index + 1, size);
 }
 
 int Binary_Search(int key, int arr[], int size) {
@@ -27,19 +28,18 @@ int Binary_Search(int key, int arr[], int size) {
         }
         if (arr[mid] < key) {
             left = mid + 1;
-        }
-        else{
+        } else {
             right = mid - 1;
         }
-       
-
     }
     return -1;
 }
-int Recursive_Binary_Search(int key, int arr[],int left,int right ) {
-    if (left > right) return-1;
+
+int Recursive_Binary_Search(int key, int arr[], int left, int right) {
+    if (left > right) return -1;
     int mid = left + (right - left) / 2;
-    if (arr[mid] < key) return Recursive_Binary_Search(key, arr, mid + 1, right );
+
+    if (arr[mid] < key) return Recursive_Binary_Search(key, arr, mid + 1, right);
     if (arr[mid] > key) return Recursive_Binary_Search(key, arr, left, mid - 1);
     else {
         return mid;
@@ -48,11 +48,11 @@ int Recursive_Binary_Search(int key, int arr[],int left,int right ) {
 }
 
 int main() {
-    int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int index = Sequential_Search(4, arr, 10);
     int index2 = Recursive_Sequential_Search(5, arr, 0, 10);
     int index3 = Binary_Search(5, arr, 10);
-    int index4 = Recursive_Binary_Search(2, arr, 0, 10);
+    int index4 = Recursive_Binary_Search(2, arr, 0, 9);
 
     cout << index << endl;
     cout << index2 << endl;
